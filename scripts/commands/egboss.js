8dd55d051560464a7d1008b43198441a84597579
BOT-SADIYA-420 <iskabbo641@gmail.com>
2026-04-02 06:46:20 +0300

    Update egboss.js

--- scripts/commands/egboss.js
+++ scripts/commands/egboss.js
@@ -18,7 +18,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   const body = content.toLowerCase();
   if (body.indexOf("kabbo")==0 || body.indexOf("KABBO")==0 || body.indexOf("kabbo")==0 || body.indexOf("farhan")==0) {
     var msg = {
-        body: "👉আমার বস♻️ kabbo এখন বিজি আছে । তার ইনবক্সে এ মেসেজ দিয়ে রাখো ‎‎‎‎‎‎‎‎‎https://www.facebook.com/kuang.39179🔰 ♪√বস ফ্রি হলে আসবে🧡😁😜🐒",
+        body: "👉আমার বস♻️ kabbo এখন বিজি আছে💔 । তার ইনবক্সে এ মেসেজ দিয়ে রাখো ‎‎‎‎‎‎‎‎‎https://www.facebook.com/kuang.39179🔰 ♪√বস ফ্রি হলে আসবে🧡😁😜🐒",
         attachment: fs.createReadStream(__dirname + `/Nayan/boss.mp3`)
       }
       api.sendMessage( msg, threadID, messageID);
